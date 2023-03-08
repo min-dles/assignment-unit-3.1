f1525e364ddd528fe4f7e20169068771e1908e89
mandi andrade <mandi.andrade20@gmail.com>
2023-03-08 17:00:20 -0600

    completed 4a-4d questions

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -49,8 +49,8 @@ animalArray.push('penguin');
 console.log('Added an animal to end,', animalArray);
 
 // 4.a. TODO: Add a new food at the end of your array & log the array
-
-
+faveFoods.push('donuts');
+console.log('Added a fave food to the end:', faveFoods);
 
 // Example: Remove the last animal by using Array.pop
 let removedAnimal = animalArray.pop();
@@ -59,14 +59,17 @@ console.log('The animals are now', animalArray);
 
 // 4.b. TODO: Remove the food at the end of your array & 
 //      log both the food removed and the updated array
-
+let removedFood = faveFoods.pop();
+console.log('JK, removed the last food:', removedFood);
+console.log('The fave foods are again:', faveFoods);
 
 // Example: Add an animal to the beginning using Array.unshift
 animalArray.unshift('walrus');
 console.log(`Added an animal to beginning: ${animalArray}`);
 
 // 4.c. TODO: Add a food at the beginning of the array & log the array
-
+faveFoods.unshift('carrots');
+console.log(`Added a new fave food to the beginning: ${faveFoods}`);
 
 // Example: Remove the first animal using Array.shift
 removedAnimal = animalArray.shift();
@@ -75,6 +78,9 @@ console.log('The animals are now', animalArray);
 
 // 4.d TODO: Remove the food at the beginning of your array & 
 //     log both the food removed and the updated array
+removedFood = faveFoods.shift()
+console.log(`Removed the first food: ${removedFood}`);
+console.log(`The fave foods are once again: ${faveFoods}`);
 
 // 4.e (STRETCH) TODO: Replace the second food in your array
 //      with another one of your favorite foods.
