abc3ff2ae8a971d773635cc35b807eb10bcc02c8
mandi andrade <mandi.andrade20@gmail.com>
2023-03-08 18:47:16 -0600

    completed stretch goals 4e-4h

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -85,16 +85,23 @@ console.log(`The fave foods are once again: ${faveFoods}`);
 // 4.e (STRETCH) TODO: Replace the second food in your array
 //      with another one of your favorite foods.
 //      Then log the updated array.
+faveFoods[1] = 'eggs'
+console.log('Here is an updated list of my fave foods:', faveFoods);
 
 // 4.f (STRETCH) TODO: Sort your favoriteFoods array
 //     in reverse alphabetical order. Log the array.
+faveFoods.reverse(faveFoods.sort());
+console.log(faveFoods);
 
 // 4.g (STRETCH) TODO: Convert your array to a string
 //     putting the word "and" between each item.
 //     eg "tacos and pizza and pasta". Log the string.
+console.log(faveFoods.join(" and "));
 
 // 4.h (STRETCH) TODO: Make a new array that combines 
 //     the favorite foods array with the animals array.
 //     Then log the new array.
 //     It should look something like:
 //     ['pizza', 'pasta', 'fish', 'cat', 'bird', 'dog']
+const faveThings = [...faveFoods, ...animalArray];
+console.log(faveThings);
